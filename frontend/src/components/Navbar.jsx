35c8e6f91f4ca5f5ab3494a48b8e91e99a0fe05b
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2022-05-07 16:06:40 -0700

    first

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const LinkWrapperClass = "text-center text-sm px-2 py-1 text-neutral-200 rounded
 export default function Navbar() {
   return(
     <div className="absolute w-full">
-      <div className="flex gap-2 justify-center items-center bg-cyan-900 h-12 shadow-sm shadow-neutral-800">
-        <div className={`${LinkWrapperClass} bg-cyan-700 hover:bg-cyan-600`}>
+      <div className="flex gap-2 justify-center items-center bg-neutral-900 h-12 shadow-sm shadow-neutral-800">
+        <div className={`${LinkWrapperClass} bg-neutral-700 hover:bg-neutral-600`}>
           <Link to="/">Catalogues</Link>
         </div>
-        <div className={`${LinkWrapperClass} bg-rose-700 hover:bg-rose-600`}>
+        <div className={`${LinkWrapperClass} bg-red-700 hover:bg-red-600`}>
           <Link to="/catalogues/add">Add catalogue</Link>
         </div>
       </div>
