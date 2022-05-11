b651f526a36cb206fc16283a7a16feb0ce40167c
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2022-05-11 02:42:20 -0700

    pdf-upload

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,8 +3,8 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
-      // target: "https://qrkodi.herokuapp.com",
+      // target: 'http://localhost:3001',
+      target: "https://qrkodi.herokuapp.com",
       changeOrigin: true,
     })
   );
